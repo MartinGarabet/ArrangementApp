073e67f675447700e89361936681cc596ad6ca6f
Martin Rami Louay Garabet <mrgarabe@hiof.no>
2026-09-08 14:49:14 +0200

    init: sett opp Spring Boot-prosjekt med grunnstruktur

---
+++ src/main/java/com/arrangement/ArrangementAppApplication.java
@@ -0,0 +1,12 @@
+package com.arrangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrangementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrangementAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arrangement/arrangementapp/ArrangementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrangement.arrangementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrangementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
