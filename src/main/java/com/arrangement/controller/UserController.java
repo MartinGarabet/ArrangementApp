f983d134a75cbc879bd75e9042fe53d0ce217458
Martin Rami Louay Garabet <mrgarabe@hiof.no>
2026-09-11 17:57:00 +0200

    feat: legg til passordhashing og innlogging

--- src/main/java/com/arrangement/controller/UserController.java
+++ src/main/java/com/arrangement/controller/UserController.java
@@ -2,6 +2,7 @@ package com.arrangement.controller;
 
 import com.arrangement.model.User;
 import com.arrangement.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
@@ -19,6 +22,7 @@ public class UserController {
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             throw new RuntimeException("E-post er allerede i bruk");
         }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
